1f51582960d1af94353084780fad07b4534dcc27
JuanOrozco213 <Juanorozco213@gmail.com>
2018-05-02 10:15:01 -0700

    First commit
    server side commit
    user log in, add project, add todos done

---
+++ client/app.js
@@ -0,0 +1,110 @@
+// Chat Component
+const chatComponent = {
+    template: ` <div class="chat-box">
+                   <p v-for="data in content">
+                   <span><strong>{{data.id}}</strong> <small>{{data.name}}</small><span>
+                       <br />
+                       {{data.todos}}
+                   </p>
+               </div>`,
+    props: ['content']
+}
+
+// Users Component
+const usersComponent = {
+    template: ` <div class="user-list">
+                   <h6>Active Users ({{users.length}})</h6>
+                   <ul v-for="user in users">
+                       <li>
+                       <img v-bind:src="user.avater" class="circle" width="30px">
+                       <span>{{user.name}}</span>
+                       </li>
+                       <hr>
+                   </ul>
+               </div>`,
+    props: ['users']
+}
+
+
+
+
+const socket = io()
+const app = new Vue({
+    el: '#chat-app',
+    data: {
+        loggedIn: false,
+        Faillogin: false,
+        userName: '',
+        user: {},
+        users: [],
+        ProjectName: '',
+        projects: [],
+        ProjectID:"",
+        TodoName:"",
+        TodoDes:""
+    },
+    methods: {
+        joinUser: function () {
+            if (!this.userName)
+                return
+
+            socket.emit('join-user', this.userName)
+        },
+        sendProject: function () {
+            if (!this.ProjectName)
+                return
+
+            socket.emit('send-projects', { ProjectName: this.ProjectName })
+        },
+        sendToDO: function () {
+            if (!this.ProjectID)
+                return
+            if (!this.TodoName)
+                return
+            if (!this.TodoDes)
+                return
+
+            socket.emit('send-todo', { ProjectID: this.ProjectID,TodoName: this.TodoName,TodoDes: this.TodoDes,userName:this.userName })
+        }
+    },
+    components: {
+        'users-component': usersComponent,
+        'chat-component': chatComponent,
+
+    }
+})
+
+
+// Client Side Socket Event
+socket.on('refresh-projects', projects => {
+    app.projects = projects
+})
+socket.on('refresh-users', users => {
+    app.users = users
+})
+
+socket.on('successful-join', user => {
+    // the successful-join event is emitted on all connections (open browser windows)
+    // so we need to ensure the loggedIn is set to true and user is set for matching user only
+    if (user.name === app.userName) {
+        app.user = user
+        app.loggedIn = true
+        app.Faillogin = false
+
+    }
+
+    app.users.push(user)
+})
+socket.on('failed-join', flag => {
+    if(app.loggedIn == false)
+    {
+        app.Faillogin = flag
+    }
+    
+})
+
+socket.on('successful-project', content => {
+    // clear the message after success send
+    app.ProjectName = ''
+    app.projects.push(content)
+})
---
+++ server/sockets.js
@@ -0,0 +1,85 @@
+module.exports = (server) => {
+    const
+        io = require('socket.io')(server),
+        moment = require('moment')
+
+    let users = []
+    const projects = []
+    let ProjectsCounter = 0;//id for projects
+
+    // when the page is loaded in the browser the connection event is fired
+    io.on('connection', socket => {
+
+        // on making a connection - load in the content already present on the server
+        socket.emit('refresh-projects', projects)
+        socket.emit('refresh-users', users)
+        //on logging in to the server
+        socket.on('join-user', userName => {
+            let flag = false
+            const length = users.length;
+            //checks if username is in users array
+            for(let i =0;i< length;i++)
+            {
+                if(users[i].name.toLowerCase()==userName.toLowerCase())
+                {
+                    flag = true;
+                    break;
+                }
+            }
+            if(flag == true)// if found, then emit a fail join otherwise let use join in
+            {
+                io.to(socket.id).emit('failed-join', flag)
+            }
+            else{
+                const user = {
+                id: socket.id,
+                name: userName,
+                avater:`https://robohash.org/${userName}?set=set3`
+            }
+            users.push(user)
+            io.emit('successful-join', user)
+        }
+
+        })
+        //on adding a project to server
+        socket.on('send-projects', data => {
+            const project = {
+                id: ProjectsCounter,
+                name: data.ProjectName,
+                date: moment(new Date()).format('MM/DD/YY h:mm a'),
+                todos: [],
+                ToDoIdCounter:0 //To Do task id counter 
+            }
+            ProjectsCounter++;
+            projects.push(project)
+            io.emit('successful-project', project)
+        })
+        //on adding a task to project. needs ProjectID, name for task, description for task and user name.
+        socket.on('send-todo', data => {
+            const todo = {
+                ProjectID: data.ProjectID,
+                id: projects[data.ProjectID].ToDoIdCounter,
+                name: data.TodoName,
+                description:data.TodoDes,
+                startdate: moment(new Date()).format('MM/DD/YY h:mm a'),
+                finishdate: "",
+                todos: [],
+                writtenby:data.userName,
+                finishedby:"",
+                completed: false
+
+            }
+            projects[data.ProjectID].ToDoIdCounter++;
+            projects[data.ProjectID].todos.push(todo);
+            io.emit('refresh-projects', projects)
+        })
+
+        socket.on('disconnect', () => {
+            users = users.filter(user => {
+                return user.id != socket.id
+            })
+
+            io.emit('refresh-users', users)
+        })
+    })
+}
